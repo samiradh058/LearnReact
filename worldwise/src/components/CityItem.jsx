a6ce83b21637589ad585f277d6a930eb1a4de635
samiradh058 <adhikarisamir68@gmail.com>
2024-07-04 22:43:35 +0545

    First commit worldwise folder

---
+++ worldwise/src/components/CityItem.jsx
@@ -0,0 +1,48 @@
+/*eslint-disable*/
+import { Link } from "react-router-dom";
+import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  const { currentCity, deleteCity } = useCities();
+  const { cityName, emoji, date, id, position } = city;
+
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
+
+  return (
+    <li>
+      {/* Step 2 to create params in URL 
+      
+      What to pass to next page is mentioned in "to"
+
+      When clicked on the link, the id is now shown in the URL
+      */}
+
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
+      </Link>
+    </li>
+  );
+}
+
+export default CityItem;
