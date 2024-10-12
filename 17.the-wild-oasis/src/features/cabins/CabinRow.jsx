9c7996c26712146dca14797f9faee9da6d196141
samiradh058 <adhikarisamir68@gmail.com>
2024-10-12 21:23:13 +0545

    Happy Dashain: Implement toasts

--- 17.the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 17.the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -60,12 +61,12 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert(`Cabin ${name} successfully deleted`);
+      toast.success(`Cabin ${name} successfully deleted`);
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
