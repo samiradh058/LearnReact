5daf3e4ac5bf4c107fa42539d1d60b6fdfbea708
samiradh058 <adhikarisamir68@gmail.com>
2024-10-21 22:34:50 +0545

    Nothing

--- store-management/src/pages/Dashboard.jsx
+++ store-management/src/pages/Dashboard.jsx
@@ -1,5 +1,10 @@
 function Dashboard() {
-  return <div>It is a dashboard showing the statistics</div>;
+  return (
+    <div>
+      It is a dashboard showing the statistics and basic introduction of the
+      application.
+    </div>
+  );
 }
 
 export default Dashboard;
