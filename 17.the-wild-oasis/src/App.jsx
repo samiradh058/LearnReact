b8ee1f2930283a2d8e224f5af5a2c18c9049f93c
samiradh058 <adhikarisamir68@gmail.com>
2024-10-27 13:42:04 +0545

    Checking in a booking

--- 17.the-wild-oasis/src/App.jsx
+++ 17.the-wild-oasis/src/App.jsx
@@ -15,6 +15,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
 import { Toaster } from "react-hot-toast";
+import Checkin from "./pages/Checkin";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -38,6 +39,7 @@ function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
             <Route path="bookings/:bookingId" element={<Booking />} />
+            <Route path="checkin/:bookingId" element={<Checkin />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
