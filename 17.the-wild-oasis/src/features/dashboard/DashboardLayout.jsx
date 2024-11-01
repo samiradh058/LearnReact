74f8f77124bd52e5b778c969a9686dd6f399513b
samiradh058 <adhikarisamir68@gmail.com>
2024-11-01 11:24:35 +0545

    Dark mode, compute recent bookings and stays

--- 17.the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 17.the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,22 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+  console.log(bookings, stays, confirmedStays);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today acvi</div>
+      <div>Cgart</div>
+      <div>cart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- 17.the-wild-oasis/src/ui/HeaderMenu.jsx
+++ 17.the-wild-oasis/src/ui/HeaderMenu.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logout from "../features/authentication/Logout";
 import ButtonIcon from "./ButtonIcon";
+import DarkModeToggle from "./DarkModeToggle";
 import { HiOutlineUser } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 
@@ -21,6 +22,9 @@ function HeaderMenu() {
       </li>
       <li>
         <Logout />
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
     </StyledHeaderMenu>
   );
